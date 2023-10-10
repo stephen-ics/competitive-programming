2ec10ac7653fac11f1263ed3ab4533f76815a9c1
stephen-ics <stephenni1234@gmail.com>
2023-10-09 23:12:13 -0400

    TEX Quotes - Solved

--- Chapter1-Introduction/Ad-Hoc-Problems/TEX Quotes.cpp
+++ Chapter1-Introduction/Ad-Hoc-Problems/TEX Quotes.cpp
@@ -2,12 +2,29 @@
 // Created by Stephen Ni on 2023-10-09.
 //
 
+// Link to the Problem: https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=4&page=show_problem&problem=208
+
 #import <iostream>
 #import <string>
 
 int main() {
     std::string sentence;
+    std::getline(std::cin, sentence);
 
-    std::cout << "hello!";
+    bool first = true;
+    for (int i = 0; i < sentence.length(); i++) {
+        if (sentence[i] == '\"') {
+            if (first) {
+                sentence.replace(i, 1, "``");
+                first = false;
+            } else {
+                sentence.replace(i, 1, "''");
+                first = true;
+
+            }
+        }
+    }
+
+    std::cout << sentence;
     return 0;
 }
