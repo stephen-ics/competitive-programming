f240b7a4d87d0f203346ea26bd60143ccea859df
stephen-ics <stephenni1234@gmail.com>
2023-10-11 15:47:24 -0400

    two sum problem!

---
+++ Chapter1-Introduction/Ad-Hoc-Problems/TwoSum.cpp
@@ -0,0 +1,33 @@
+//
+// Created by Stephen Ni on 2023-10-10.
+//
+
+#include <iostream>
+#include <vector>
+
+int main() {
+
+    int arr[] = {3, 2, 4};
+    std::vector<int> nums(arr, arr + sizeof(arr) / sizeof(arr[0]));
+
+    int target = 6;
+
+    for (int i = 0; i < nums.size(); i++) {
+        int num = nums[i];
+        int sum = target - num;
+        for (int j = i+1; j < nums.size(); j++) {
+            int num2 = nums[j];
+            if (num2 == sum) {
+                std::cout << i << " " << j << std::endl;
+            }
+        }
+    }
+
+    return 0;
+}
+
+// start from a number
+// sum = target - num
+// pop num from list
+// check if sum is in the list
+// rinse and repreat
